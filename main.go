ede1e0748ea001189c311c1d0a5e0ffa0075c442
vick <lianghuiming@luojilab.com>
2019-07-01 11:41:00 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
-	"appstore"
+	"time"
+
+	"github.com/vickxxx/appstore/appstore"
 )
 
 func main() {
 	fmt.Println("xx")
-	appstore.GenFYDetail(2019)
+	fy2019 := appstore.GenFY(2019)
+	fmt.Println(fy2019)
+
+	ac := appstore.AppleClient{
+		KeyID:    "KeyID",
+		IssID:    "IssID",
+		VendorNO: "VendorNO",
+		PrivKey:  "PrivKey",
+	}
+	ac.Init()
+
+	fy201905 := time.Date(2019, time.May, 1, 0, 0, 0, 0, time.UTC)
+
+	ac.GetFinanceStat(fy201905) // 2019财年p5财务数据
+
+	ac.GetSalesStatInFYP(fy201905) //  2019财年p5销售数据
+
+	ac.GetAppleSales("20190622", "DAILY") // 2019年6月22日销售数据
+
 	// appstore.GenFYDetail()
 }
